f64f712318aa64ce496013e55fe14081504ed8c1
tayta155 <sustayta_15@hotmail.com>
2024-09-26 16:39:47 -0600

    first commit

---
+++ src/main/java/sanmina/serials/sanmina_serials/SanminaSerialsApplication.java
@@ -0,0 +1,13 @@
+package sanmina.serials.sanmina_serials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanminaSerialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanminaSerialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sanmina/serials/sanmina_serials/SanminaSerialsApplicationTests.java
@@ -0,0 +1,13 @@
+package sanmina.serials.sanmina_serials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanminaSerialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
