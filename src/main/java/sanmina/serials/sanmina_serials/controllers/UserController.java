32ff4b9c2b7f1697ce7626e3e9926cd194dbea0a
tayta155 <sustayta_15@hotmail.com>
2024-09-29 01:01:13 -0600

    espacio

--- src/main/java/sanmina/serials/sanmina_serials/controllers/UserController.java
+++ src/main/java/sanmina/serials/sanmina_serials/controllers/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
-    @GetMapping("/details ")
+    @GetMapping("/details")
     public String details(){
 
         return "details";
