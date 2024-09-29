35ec79e2a43da03ca0a133ecfd4cb03b555c94d5
tayta155 <sustayta_15@hotmail.com>
2024-09-29 00:45:51 -0600

    refresh

--- src/main/java/sanmina/serials/sanmina_serials/controllers/UserController.java
+++ src/main/java/sanmina/serials/sanmina_serials/controllers/UserController.java
@@ -1,5 +1,14 @@
 package sanmina.serials.sanmina_serials.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
 
+    @GetMapping("/details ")
+    public String details(){
+
+        return "Details";
+    }
 }
