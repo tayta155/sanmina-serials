d83872af6f7c5d9fe9da613d2474ac0b6664769c
tayta155 <sustayta_15@hotmail.com>
2024-09-30 23:30:51 -0600

    cambios repentinos

--- src/main/java/sanmina/serials/sanmina_serials/controllers/UserController.java
+++ src/main/java/sanmina/serials/sanmina_serials/controllers/UserController.java
@@ -1,13 +1,16 @@
 package sanmina.serials.sanmina_serials.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
     @GetMapping("/details")
-    public String details(){
+    public String details(Model model){
+
+        model.
 
         return "details";
     }
